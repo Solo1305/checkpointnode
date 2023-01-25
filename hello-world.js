83877dbd878692c842e7288ef3a2bde43b0defa8
Solo1305 <116720176+Solo1305@users.noreply.github.com>
2023-01-25 10:47:17 +0100

    Add files via upload

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+console.log("HELLO WORLD");
+var http = require('http');
+
+var server = http.createServer(function (req, res) {   
+    
+
+    
+
+});
+
+server.listen(3000); 
+
+console.log('Node.js web server at port 3000 is running..')
